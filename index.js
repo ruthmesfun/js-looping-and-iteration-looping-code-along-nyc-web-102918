6d461e98543be14596349fa494ef8d1012536cae
ruthmesfun <ruth.mesfun@gmail.com>
2018-12-09 08:52:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+let printBadges = (array) => {
+    for(let i = 0; i < array.length; i++){
+        console.log(`Welcome ${array[i]}! You are employee #${i+1}.`);
+    }
+    return array
+}
+
+let tailsNeverFails = () => {
+
+    let tails = 0;
+
+    while (Math.random() >= 0.5 ){
+        tails++;
+    };
+
+    return `You got ${tails} tails in a row!`;
+}
